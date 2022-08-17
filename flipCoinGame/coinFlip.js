21e572c0e348d43db22106d69b1eb8d25d2a1cbc
jbriseno13 <104550107+jbriseno13@users.noreply.github.com>
2022-08-16 23:36:45 -0700

    CSS, HTML, and JS completed

--- flipCoinGame/coinFlip.js
+++ flipCoinGame/coinFlip.js
@@ -6,12 +6,13 @@ const coin = document.querySelector(".coinimages");
 const flipBtn = document.querySelector("#coinflip-button"); 
 const resetBtn = document.querySelector("#reset-button"); 
 
-
+//at click coin will randomly be heads or tails
 flipBtn.addEventListener("click", () => {
     let i = Math.floor(Math.random() * 2);
     console.log(i); 
     coin.style.animation = "none";
     if(i){
+        //section will spin the coin based on css animations
         setTimeout(function(){
             coin.style.animation = "spin-heads 3s forwards";
         }, 100);
@@ -26,6 +27,9 @@ flipBtn.addEventListener("click", () => {
     setTimeout(updateStats, 3000);
     disableButton();
 });
+
+//if coin is head, head count is updated
+//if coin is tail, tail count stat is updated 
 function updateStats(){
     document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
     document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
@@ -36,6 +40,8 @@ function disableButton(){
         flipBtn.disabled = false;
     },3000);
 }
+
+//reset the stats back to zero when reset button is pressed. 
 resetBtn.addEventListener("click",() => {
     coin.style.animation = "none";
     heads = 0;
